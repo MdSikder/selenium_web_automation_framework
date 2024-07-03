96748af5d747f9869563387b59e6c8c094c7844a
MdSikder <mdporagbsmrstu@gmail.com>
2024-07-03 16:06:19 +0600

    config_reader.py: Read environment-specific configurations.

---
+++ config/config_reader.py
@@ -0,0 +1,10 @@
+# Read environment-specific configurations.
+
+import yaml
+import os
+
+
+def read_config(env='default'):
+    with open(os.path.join(os.path.dirname(__file__), 'config.yaml'), 'r') as file:
+        config = yaml.safe_load(file)
+    return config['environments'].get(env, config['environments']['default'])
