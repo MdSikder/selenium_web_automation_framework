d909845c92cef7516978180ad9fad58ab021ede9
MdSikder <mdporagbsmrstu@gmail.com>
2024-07-03 16:06:43 +0600

    Enhance logging with more details and rotating file handler.

---
+++ src/utilities/logger.py
@@ -0,0 +1,19 @@
+# Enhance logging with more details and rotating file handler.
+
+import logging
+import os
+from logging.handlers import RotatingFileHandler
+
+
+class LogGen:
+    @staticmethod
+    def loggen():
+        if not os.path.exists('logs'):
+            os.makedirs('logs')
+        logger = logging.getLogger()
+        handler = RotatingFileHandler('logs/test.log', maxBytes=2000, backupCount=5)
+        formatter = logging.Formatter('%(asctime)s: %(levelname)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        logger.setLevel(logging.INFO)
+        return logger
